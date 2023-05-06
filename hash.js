d3ec616b249418f15e2d5d77aa8829bc79ebbdf1
Hans Hüppelshäuser <hans.hueppelshaeuser@icloud.com>
2023-05-06 17:29:47 +0200

    first commit

---
+++ hash.js
@@ -0,0 +1,13 @@
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:7545');
+
+const message = "Some message to sign";
+const messageHash = web3.utils.sha3(message);
+
+const privateKey = "<private key>";
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+const signatureObject = account.sign(messageHash);
+
+// Get the last 32 bytes of the signature (r, s)
+const signatureBytes32 = '0x' + signatureObject.signature.slice(-64);
+console.log(signatureBytes32);
